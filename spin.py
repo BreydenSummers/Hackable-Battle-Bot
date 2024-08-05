fd8ee6521bf3836b9722985d2dea7776414a58ef
Breyden <breydensummers@gmail.com>
2024-08-04 18:15:35 -0600

    add stopping functionality

--- spin.py
+++ spin.py
@@ -1,8 +1,13 @@
 from adafruit_motorkit import MotorKit
-
+import atexit
 
 kit = MotorKit()
 
+
+@atexit.register
+def stop():
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
 
 while True:
     kit.motor1.throttle = 0.5
