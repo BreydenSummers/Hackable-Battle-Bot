ba5386ef126e78ea21eac92261bf28201b6c6548
Breyden <breydensummers@gmail.com>
2024-08-05 20:44:11 -0600

    oof

--- spin.py
+++ spin.py
@@ -12,10 +12,10 @@ def stop():
     kit.motor2.throttle = 0
 
 while True:
-    kit.motor1.throttle = 0.25
-    kit.motor2.throttle = 0.25
+    kit.motor1.throttle = 0.50
+    kit.motor2.throttle = 0.50
     time.sleep(1) 
-    kit.motor1.throttle = -0.25
-    kit.motor2.throttle = -0.25
+    kit.motor1.throttle = -0.50
+    kit.motor2.throttle = -0.50
     time.sleep(1) 
     signal.signal(signal.SIGTERM, stop)
